dbd108d986158b65124792df322c7066fa060200
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-25 02:04:18 +0300

    assigns a new class name

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-function renderPhoneAccessories(phoneAccessories){
-    const card=document.createElement('div');
-    card.classList.add("phoneAccessories");
-    card.innerHTML=`
+function renderPhoneAccessories(phoneAccessories) {
+  const card = document.createElement("div");
+
+  //creating a card
+
+  card.classList.add("phoneAccessories");
+  //innerHTML takes a new card and gives a short explanation of phone accessories.
+  //innerHTML assigns a new name.
+  card.innerHTML = `
     <p>${phoneAccessories.name}</p>
     <p>${phoneAccessories.brand}</p>
     <p>${phoneAccessories.description}</p>
     <img src=${phoneAccessories.image}/>`;
-    document.getElementById("phoneAccessoriesList").append(card);
-
+  document.getElementById("phoneAccessoriesList").append(card);
 }
-function displayAccessory(){
-    fetch( "http://localhost:3000/phone-accessory")
-    .then(res =>res.json())
-    .then(phoneAccessories =>phoneAccessories.forEach(renderPhoneAccessories))
+function displayAccessory() {
+  fetch("http://localhost:3000/phone-accessory")
+    .then((res) => res.json())
+    .then((phoneAccessories) =>
+      phoneAccessories.forEach(renderPhoneAccessories)
+    );
 }
-document.addEventListener("DOMContentLoaded",function(){
-    displayAccessory();
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+  displayAccessory();
+});
