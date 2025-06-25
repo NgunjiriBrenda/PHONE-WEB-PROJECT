476f521b7df1392abecc5ec71eb24013545c8e4d
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-25 15:48:05 +0300

    styles title

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function renderPhoneAccessories(phoneAccessories) {
+function renderPhoneAccessories(phoneAccessory) {
   const card = document.createElement("div");
 
   //creating a card
@@ -7,14 +7,17 @@ function renderPhoneAccessories(phoneAccessories) {
   //innerHTML takes a new card and gives a short explanation of phone accessories.
   //innerHTML assigns a new name.
   card.innerHTML = `
-    <p>${phoneAccessories.name}</p>
-    <p>${phoneAccessories.brand}</p>
-    <p>${phoneAccessories.description}</p>
-    <img src=${phoneAccessories.image}/>`;
+    <p class= "title">${phoneAccessory.name}</p> 
+    <p>${phoneAccessory.brand}</p>
+    <p>${phoneAccessory.description}</p>
+    <img src=${phoneAccessory.image} style ="width: 100%; height: auto;"/>`;
+  //creates a new element
   document.getElementById("phoneAccessoriesList").append(card);
 }
+//get datas as objects
 function displayAccessory() {
   fetch("http://localhost:3000/phone-accessory")
+    //filtering responses
     .then((res) => res.json())
     .then((phoneAccessories) =>
       phoneAccessories.forEach(renderPhoneAccessories)
